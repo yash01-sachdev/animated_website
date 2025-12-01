5d274c203c27409ee8ff26c623075d3be419b75d
yash01-sachdev <yashsachdev2003@gmail.com>
2025-12-02 03:46:28 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-})
+    base: "/animated_website/",
+  })
+  
+
