61e0a2e8a1701cf00030661774c6334c1b3b47dc
yash01-sachdev <yashsachdev2003@gmail.com>
2025-12-04 02:29:30 +0530

    Fix base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/animated_website/",
-  build: {
-    assetsInlineLimit: 0,         // disables automatic bundling of fonts
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) => {
-          if (/\.woff2$/.test(assetInfo.name)) {
-            return 'fonts/[name][extname]';  // put fonts in a separate folder
-          }
-          return 'assets/[name]-[hash][extname]';
-        }
-      }
-    }
-  }
+  base: "/",   // IMPORTANT for Netlify
 })
